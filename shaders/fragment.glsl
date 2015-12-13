8b00d0ee007bf4a64e597a818c99fd680f5770a4
Jonathan Castello <twisolar@gmail.com>
2015-12-12 18:28:14 -0800

    Prepare lighting to be parametrized via uniforms

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -5,9 +5,11 @@ struct Light {
   vec3 color;
 
   float ambient;  // Approximates scattering for this light
+  float attenuation;
+  bool enabled;  // Whether the light should be utilized
 };
 
-const Light ruberLight = Light(vec3(0, 0, 0), vec3(1, 1, 1), 0.2);
+const Light ruberLight = Light(vec3(0, 0, 0), vec3(1, 1, 1), 0.2, 0.000000003, true);
 const float ambient = 0.3;  // Global ambient light
 
 in vec3 position;
@@ -18,13 +20,17 @@ in vec4 emissivity;
 layout(location=0) out vec4 fragColor;
 
 vec4 applyLighting(Light light) {
+  if (!light.enabled) {
+    return vec4(0, 0, 0, 1);
+  }
+
   vec3 to_light = light.position - position;
   float len = length(to_light);
   to_light = normalize(to_light);
 
   float diffuseFactor = max(0, dot(normal, to_light));
   float ambientFactor = light.ambient;
-  float attenuation = 1.0/(1.0 + 0.000000003*len*len);
+  float attenuation = 1.0/(1.0 + light.attenuation*len*len);
 
   return (ambientFactor + diffuseFactor) * attenuation * color * vec4(light.color, 1);
 }
